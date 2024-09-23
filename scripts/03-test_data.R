828af28903d5a9adc27e1698806f4392246b84a2
karenrni <clairriani@gmail.com>
2024-09-22 22:31:01 -0400

    Initial Format Completed

---
+++ scripts/03-test_data.R
@@ -0,0 +1,26 @@
+#### Preamble ####
+# Purpose: Sanity check of the data
+# Author: Karen Riani
+# Date: 19 September 2024
+# Contact: karen.riani@mail.utoronto.ca
+# Pre-requisites: None
+# Any other information needed? None
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$number_of_marriage |> min() <= 0
+
+# Test for NAs
+all(is.na(data$number_of_marriage))
+
+
+
+
+
+
