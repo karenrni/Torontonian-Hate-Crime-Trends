25d2f51add5ea74c4c6409c942646f1860b1c5d1
karenrni <clairriani@gmail.com>
2024-09-24 14:01:16 -0400

    Mutated race and offence groups

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the raw marriage data into an analysis dataset
 # Author: Karen Riani
-# Date: 19 September 2024
+# Date: 22 September 2024
 # Contact: karen.riani@mail.utoronto.ca
 # Pre-requisites: None
 # Any other information needed? None
@@ -12,14 +12,20 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/raw_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |> 
-  separate(col = time_period,
-            into = c("year", "month"),
-            sep = "-") |> 
-  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-"))
-         )
-  
+# Recategorize race and primary offences
+cleaned_data <- data %>%
+  mutate(
+    race_bias_recategorized = case_when(
+      str_detect(RACE_BIAS, "East/Southeast Asian") ~ "East Asian mixed",
+      str_detect(RACE_BIAS, "South Asian") ~ "South Asian mixed",
+      TRUE ~ "All the rest"
+    ),
+    primary_offence_recategorized = case_when(
+      PRIMARY_OFFENCE %in% c("Assault", "Aggravated Assault") ~ "Assault",
+      PRIMARY_OFFENCE %in% c("Mischief Under $5000", "Mischief Over $5000") ~ "Mischief",
+      TRUE ~ "Other"
+    )
+  )
+
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
