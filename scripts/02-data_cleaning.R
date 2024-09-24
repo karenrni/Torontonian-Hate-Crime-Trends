92d2c4c07b244725d8db27ba62aef9e64381f632
karenrni <clairriani@gmail.com>
2024-09-24 15:26:56 -0400

    Group by offence, remove racial grouping

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -14,18 +14,15 @@ raw_data <- read_csv("data/raw_data/raw_data.csv")
 
 # Recategorize race and primary offences
 cleaned_data <- data %>%
-  mutate(
-    race_bias_recategorized = case_when(
-      str_detect(RACE_BIAS, "East/Southeast Asian") ~ "East Asian mixed",
-      str_detect(RACE_BIAS, "South Asian") ~ "South Asian mixed",
-      TRUE ~ "All the rest"
-    ),
-    primary_offence_recategorized = case_when(
-      PRIMARY_OFFENCE %in% c("Assault", "Aggravated Assault") ~ "Assault",
-      PRIMARY_OFFENCE %in% c("Mischief Under $5000", "Mischief Over $5000") ~ "Mischief",
-      TRUE ~ "Other"
-    )
-  )
+  mutate(OFFENCE_GROUP = case_when(
+    PRIMARY_OFFENCE %in% c("Assault", "Assault With a Weapon", "Assault Causing Bodily Harm", 
+                           "Aggravated Assault", "Assault Peace Officer", "Sexual Assault", "Murder") ~ "Assault-related Offenses",
+    PRIMARY_OFFENCE %in% c("Mischief Under $5000", "Mischief Interfere With Property", "Mischief To Religious Property, Educational Institutions, Etc.",
+                           "Mischief Over $5000", "Mischief To Data", "Arson") ~ "Mischief and Property Damage",
+    PRIMARY_OFFENCE %in% c("Uttering Threats - Bodily Harm", "Uttering Threats - Property", "Criminal Harassment", "Harassing Communications") ~ "Threats and Harassment",
+    PRIMARY_OFFENCE %in% c("Wilful Promotion of Hatred", "Public Incitement of Hatred", "Advocating Genocide", "Disturbing Religious Worship Or Certain Meetings") ~ "Hate-related Offenses",
+    TRUE ~ "Other Offenses"
+  ))
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
