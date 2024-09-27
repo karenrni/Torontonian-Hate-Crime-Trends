7af200a9bf4ee7e42d049930cba027f6f74a0d60
karenrni <clairriani@gmail.com>
2024-09-27 14:41:36 -0400

    Additional cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the raw marriage data into an analysis dataset
 # Author: Karen Riani
-# Date: 22 September 2024
+# Date: 27 September 2024
 # Contact: karen.riani@mail.utoronto.ca
 # Pre-requisites: None
 # Any other information needed? None
@@ -12,8 +12,21 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/raw_data.csv")
 
-# Recategorize race and primary offences
-cleaned_data <- data %>%
+# Ensure correct date format
+raw_data$OCCURRENCE_DATE <- as.Date(raw_data$OCCURRENCE_DATE, format = "%Y-%m-%d")
+
+# Check duplicates based on _id
+duplicate_rows <- raw_data[duplicated(raw_data$EVENT_UNIQUE_ID), ]
+
+# Remove duplicates
+data_cleaned <- raw_data[!duplicated(raw_data$EVENT_UNIQUE_ID), ]
+
+# Rename "Other" to "Unknown" in the RACE_BIAS column
+data <- data %>%
+  mutate(RACE_BIAS = recode(RACE_BIAS, "Other race not mentioned" = "Unknown"))
+
+# Consolidate primary offence types to capture broader categories
+cleaned_data <- raw_data %>%
   mutate(OFFENCE_GROUP = case_when(
     PRIMARY_OFFENCE %in% c("Assault", "Assault With a Weapon", "Assault Causing Bodily Harm", 
                            "Aggravated Assault", "Assault Peace Officer", "Sexual Assault", "Murder") ~ "Assault-related Offenses",
